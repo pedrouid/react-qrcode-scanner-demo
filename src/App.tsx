0405cdd5189c6f6f9dac59f3ba98677605cf5eb8
Pedro Gomes <pedrogomes94@gmail.com>
2018-12-22 00:04:47 +0000

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,119 @@
 import * as React from "react";
 import styled from "styled-components";
+import Button from "./components/Button";
 import QRCodeScanner from "./components/QRCodeScanner";
 import { colors } from "./styles";
 
-const SApp = styled.div`
+const SContainer = styled.div`
   text-align: center;
 `;
 
-const SAppHeader = styled.header`
+const SHeader = styled.header`
   background-color: rgb(${colors.grey});
   padding: 10px;
   color: rgb(${colors.white});
 `;
 
-const SAppTitle = styled.h1`
+const SHero = styled.h1`
   margin: 20px auto;
 `;
 
-const SAppContent = styled.div`
+const STitle = styled.h5`
+  font-weight: bold;
+  margin: 30px auto;
+`;
+
+const SContent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
+const SFlex = styled.div`
+  width: 100%;
+  max-width: 600px;
+  padding: 30px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SMonospace = styled.div`
+  width: 100%;
+  font-family: monospace;
+  margin: 30px auto;
+  padding: 30px 60px;
+  word-break: break-word;
+
+  border: 1px solid rgba(${colors.dark}, 0.3);
+  border-radius: 6px;
+`;
+
+interface IAppState {
+  scan: boolean;
+  result: any | null;
+}
+
 class App extends React.Component {
+  public state: IAppState;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      scan: false,
+      result: null
+    };
+  }
+
+  public resetApp = () => this.setState({ scan: false, result: null });
+
+  public startScan = () => this.setState({ scan: true });
+
+  public onValidate = (result: any) => {
+    if (result && typeof result === "string") {
+      return true;
+    }
+    return false;
+  };
+
+  public onScan = (result: any) => {
+    if (result) {
+      this.setState({ scanner: false, result });
+    }
+  };
+
+  public onClose = () => this.setState({ scanner: false });
+
   public render() {
     return (
-      <SApp>
-        <SAppHeader>
-          <SAppTitle>React QRCode Scanner</SAppTitle>
-        </SAppHeader>
-        <SAppContent>
-          <QRCodeScanner />
-        </SAppContent>
-      </SApp>
+      <SContainer>
+        <SHeader>
+          <SHero>React QRCode Scanner</SHero>
+        </SHeader>
+        <SContent>
+          {!this.state.result ? (
+            !this.state.scan ? (
+              <SFlex>
+                <STitle>{"Press scan to toggle QR Code scanner"}</STitle>
+                <Button onClick={this.startScan}>{"Scan"}</Button>
+              </SFlex>
+            ) : (
+              <QRCodeScanner
+                onScan={this.onScan}
+                onValidate={this.onValidate}
+                onClose={this.onClose}
+              />
+            )
+          ) : (
+            <SFlex>
+              <SMonospace>{this.state.result}</SMonospace>
+              <Button onClick={this.resetApp}>{"Reset"}</Button>
+            </SFlex>
+          )}
+        </SContent>
+      </SContainer>
     );
   }
 }
--- src/components/QRCodeScanner.tsx
+++ src/components/QRCodeScanner.tsx
@@ -1,61 +1,87 @@
 import * as React from "react";
-import styled from "styled-components";
 import * as Webcam from "react-webcam";
 import QrCode from "qrcode-reader";
-import Button from "./Button";
 
-const SQRCodeScanner = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
+interface IQRCodeScannerState {
+  interval: number | null;
+}
 
-class QRCodeScanner extends React.Component {
+interface IQRCodeScannerProps {
+  onScan: any;
+  onValidate: any;
+  onClose: any;
+}
+
+class QRCodeScanner extends React.Component<IQRCodeScannerProps> {
+  public state: IQRCodeScannerState;
   public webcam: Webcam | null;
-
+  public intervalId: any;
   public qr: any;
 
-  public componentDidMount() {
-    this.qr = new QrCode();
-    this.qr.callback = (error: Error, result: any) => {
-      if (error) {
-        console.log(error); // tslint:disable-line
-        return;
-      }
-      console.log("result", result); // tslint:disable-line
-      console.log("result.result", result.result); // tslint:disable-line
+  constructor(props: IQRCodeScannerProps) {
+    super(props);
+    this.state = {
+      interval: 250
     };
   }
 
-  public capture = () => {
+  public componentDidMount() {
+    this.qr = new QrCode();
+    this.qr.callback = (error: Error, response: any) => {
+      if (error) {
+        return;
+      }
+      const result = response.result;
+      const valid = this.props.onValidate(result);
+      if (valid) {
+        this.props.onScan(result);
+      }
+    };
+    this.startInterval();
+  }
+
+  public startInterval = () => {
+    const interval = this.state.interval || 0;
+    this.intervalId = setInterval(() => {
+      if (this.state.interval) {
+        this.scan();
+      } else {
+        clearInterval(this.intervalId);
+      }
+    }, interval);
+  };
+
+  public stopInterval = () => {
+    this.setState({ interval: null });
+  };
+
+  public scan = () => {
     if (this.webcam) {
       const imageSrc = this.webcam.getScreenshot();
-      console.log("imageSrc", imageSrc); // tslint:disable-line
       if (imageSrc) {
-        const result = this.qr.decode(imageSrc);
-        console.log("result", result); // tslint:disable-line
+        this.qr.decode(imageSrc);
       }
     }
   };
 
+  public close = () => {
+    this.stopInterval();
+    this.props.onClose();
+  };
+
+  public componentWillUnmount() {
+    this.stopInterval();
+  }
+
   public render() {
     return (
-      <SQRCodeScanner>
-        <Webcam
-          audio={false}
-          height={350}
-          ref={c => (this.webcam = c)}
-          screenshotFormat="image/jpeg"
-          width={350}
-          // videoConstraints={{
-          //   width: 1280,
-          //   height: 720,
-          //   facingMode: "user"
-          // }}
-        />
-        <Button onClick={this.capture}>Capture photo</Button>
-      </SQRCodeScanner>
+      <Webcam
+        audio={false}
+        height={350}
+        ref={c => (this.webcam = c)}
+        screenshotFormat="image/jpeg"
+        width={350}
+      />
     );
   }
 }
